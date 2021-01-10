9b7a486f6d9f5a7de097e576b33069964674e06f
nshcore <nshcore@protonmail.com>
2021-01-10 00:39:53 +0000

    ci: setup workflow to publish npm

--- index.js
+++ index.js
@@ -9,14 +9,16 @@ module.exports = {
         'import',
         'import-newlines',
         'promise',
-        'async-await'
+        'async-await',
+        'jest'
     ],
     'extends': [
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:import/errors',
         'plugin:import/warnings',
-        'plugin:import/typescript'
+        'plugin:import/typescript',
+        'plugin:promise/recommended'
     ],
     'root': true,
     'env': {
@@ -24,29 +26,117 @@ module.exports = {
         'jest': true,
     },
     'rules': {
-        'async-await/space-after-async': 2,
-        'async-await/space-after-await': 2,
-        'promise/always-return': 'error',
-        'promise/no-return-wrap': 'error',
-        'promise/param-names': 'error',
-        'promise/catch-or-return': 'error',
-        'promise/no-native': 'off',
-        'promise/no-nesting': 'warn',
-        'promise/no-promise-in-callback': 'warn',
-        'promise/no-callback-in-promise': 'warn',
-        'promise/avoid-new': 'warn',
-        'promise/no-new-statics': 'error',
-        'promise/no-return-in-finally': 'warn',
-        'promise/valid-params': 'warn',
-        'quotes': [
+        complexity: 'warn',
+        radix: 'error',
+        yoda: 'error',
+
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/no-explicit-any': 'error',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-unused-vars': 'error',
+        '@typescript-eslint/ban-types': 'error',
+        '@typescript-eslint/explicit-function-return-type': ['error'],
+        '@typescript-eslint/explicit-member-accessibility': ['error'],
+        '@typescript-eslint/require-await': 'error',
+        '@typescript-eslint/semi': ['error'],
+        '@typescript-eslint/naming-convention': [
             'error',
-            'single',
             {
-                'avoidEscape': true,
-                'allowTemplateLiterals': true,
+                'selector': 'interface',
+                'format': ['PascalCase'],
+                'custom': {
+                    'regex': '^I[A-Z]',
+                    'match': true
+                }
+            }
+        ],
+
+        'accessor-pairs': 'error',
+        'array-callback-return': 'error',
+        'block-scoped-var': 'error',
+        'consistent-return': 'error',
+        'guard-for-in': 'error',
+        'max-classes-per-file': ['error', 1],
+        'no-alert': 'error',
+        'no-caller': 'error',
+        'no-case-declarations': 'error',
+        'no-div-regex': 'error',
+        'no-else-return': 'error',
+        'no-empty-function': [
+            'error',
+            {
+                allow: ['arrowFunctions'],
+            },
+        ],
+        'no-empty-pattern': 'error',
+        'no-eq-null': 'error',
+        'no-eval': 'error',
+        'no-extend-native': 'error',
+        'no-extra-bind': 'error',
+        'no-extra-label': 'error',
+        'no-fallthrough': 'error',
+        'no-floating-decimal': 'error',
+        'no-global-assign': 'error',
+        'no-implicit-coercion': 'error',
+        'no-implicit-globals': 'error',
+        'no-implied-eval': 'error',
+        'no-invalid-this': 'error',
+        'no-iterator': 'error',
+        'no-labels': 'error',
+        'no-lone-blocks': 'error',
+        'no-loop-func': 'error',
+        'no-magic-numbers': 0,
+        'no-multi-spaces': 'error',
+        'no-new': 'error',
+        'no-new-func': 'error',
+        'no-new-wrappers': 'error',
+        'no-octal': 'error',
+        'no-octal-escape': 'error',
+        'no-param-reassign': 'error',
+        'no-proto': 'error',
+        'no-redeclare': [
+            'error',
+            {
+                'builtinGlobals': true,
             },
         ],
         'no-console': 'error',
+        'no-unused-vars': [
+            'error',
+            {
+                'vars': 'all',
+                'args': 'after-used',
+                'ignoreRestSiblings': false,
+            },
+        ],
+        'no-return-assign': 'error',
+        'no-script-url': 'error',
+        'no-self-assign': 'error',
+        'no-self-compare': 'error',
+        'no-sequences': 'error',
+        'no-throw-literal': 'error',
+        'no-unmodified-loop-condition': 'error',
+        'no-unused-expressions': 'error',
+        'no-unused-labels': 'error',
+        'no-useless-call': 'error',
+        'no-useless-concat': 'error',
+        'no-useless-escape': 'error',
+        'no-useless-return': 'error',
+        'no-void': 'error',
+        'no-warning-comments': 'error',
+        'no-with': 'error',
+        'prefer-promise-reject-errors': 'error',
+        'require-unicode-regexp': 'error',
+        'vars-on-top': 'error',
+        'wrap-iife': 'error',
+
+        'no-irregular-whitespace': 'error',
+        'no-whitespace-before-property': 'error',
+        'no-multiple-empty-lines': 'error',
+        'no-trailing-spaces': 'error',
+        'key-spacing': [ 'error', {
+            'afterColon': true,
+        }],
         'indent': ['error', 2],
         'block-spacing': 'error',
         'linebreak-style': ['error', 'unix'],
@@ -64,16 +154,6 @@ module.exports = {
             'error',
             'always'
         ],
-        '@typescript-eslint/interface-name-prefix': 'off',
-        '@typescript-eslint/explicit-function-return-type': ['error'],
-        '@typescript-eslint/explicit-member-accessibility': ['error'],
-        'no-return-await': 'error',
-        'no-await-in-loop': 'error',
-        'require-await': 'off',
-        '@typescript-eslint/require-await': 'error',
-        '@typescript-eslint/no-explicit-any': 'error',
-        'semi': 'off',
-        '@typescript-eslint/semi': ['error'],
         'comma-dangle': [
             'error',
             {
@@ -84,20 +164,56 @@ module.exports = {
                 'functions': 'always-multiline',
             },
         ],
-        'no-unused-vars': [
+
+        'no-return-await': 'error',
+        'no-await-in-loop': 'error',
+        'require-await': 'off',
+        'semi': 'off',
+        'async-await/space-after-async': 2,
+        'async-await/space-after-await': 2,
+        'promise/always-return': 'error',
+        'promise/no-return-wrap': 'error',
+        'promise/param-names': 'error',
+        'promise/catch-or-return': 'error',
+        'promise/no-native': 'off',
+        'promise/no-nesting': 'warn',
+        'promise/no-promise-in-callback': 'warn',
+        'promise/no-callback-in-promise': 'warn',
+        'promise/avoid-new': 'warn',
+        'promise/no-new-statics': 'error',
+        'promise/no-return-in-finally': 'warn',
+        'promise/valid-params': 'warn',
+
+        'jest/consistent-test-it': ['error', {fn: 'it'}],
+        'jest/expect-expect': 'error',
+        'jest/lowercase-name': 0,
+        'jest/no-disabled-tests': 'warn',
+        'jest/no-focused-tests': 'warn',
+        'jest/no-hooks': 0,
+        'jest/no-identical-title': 'error',
+        'jest/no-jasmine-globals': 'error',
+        'jest/no-jest-import': 'error',
+        'jest/no-large-snapshots': 'error',
+        'jest/no-test-prefixes': 'error',
+        'jest/no-test-return-statement': 'error',
+        'jest/prefer-expect-assertions': 0,
+        'jest/prefer-strict-equal': 'error',
+        'jest/prefer-to-be-null': 'error',
+        'jest/prefer-to-be-undefined': 'error',
+        'jest/prefer-to-have-length': 'error',
+        'jest/prefer-inline-snapshots': 0,
+        'jest/valid-describe': 'error',
+        'jest/valid-expect-in-promise': 'error',
+        'jest/valid-expect': 'error',
+        'quotes': [
             'error',
+            'single',
             {
-                'vars': 'all',
-                'args': 'after-used',
-                'ignoreRestSiblings': false,
+                'avoidEscape': true,
+                'allowTemplateLiterals': true,
             },
         ],
-        'no-redeclare': [
-            'error',
-            {
-                'builtinGlobals': true,
-            },
-        ],
+
         'import/no-unresolved': 'error',
         'import/order': ['error', {
             'groups': [
@@ -118,28 +234,13 @@ module.exports = {
         'import/named': 'warn',
         'import/default': 'error',
         'import/no-cycle': 'error',
+        'import/namespace': 'error',
         'import/no-extraneous-dependencies': 'error',
         'import/no-dynamic-require': 'error',
         'import/export': 'error',
         'import/no-useless-path-segments': 'error',
         'import/no-unassigned-import': 'error',
         'import/no-self-import': 'error',
-        // 'import/no-restricted-paths': [ 'error', {
-        //     'zones': [
-        //         {
-        //             'target': './libs',
-        //             'from': './apps',
-        //         },
-        //         {
-        //             'target': './libs',
-        //             'from': './ui',
-        //         },
-        //         {
-        //             'target': './apps',
-        //             'from': './ui',
-        //         }
-        //     ]
-        // }],
         'import-newlines/enforce': [
             'error',
             {
@@ -159,14 +260,6 @@ module.exports = {
                 'ignoreUrls': true
             }
         ],
-        'no-multi-spaces': 'error',
-        'no-irregular-whitespace': 'error',
-        'no-whitespace-before-property': 'error',
-        'no-multiple-empty-lines': 'error',
-        'no-trailing-spaces': 'error',
-        'key-spacing': [ 'error', {
-            'afterColon': true,
-        }],
     },
     'settings': {
         'import/parsers': {
